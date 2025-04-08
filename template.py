ee159481f0d7964a92d54a859bc381aa8d42e84b
unknown <durgeshsingh12712@gmail.com>
2025-04-08 13:28:47 +0530

    Folder Struckture Added

--- template.py
+++ template.py
@@ -19,6 +19,7 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
+    "schema.yaml",
     "main.py",
     "app.py",
     "requirements.txt",
